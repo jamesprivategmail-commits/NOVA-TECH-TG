ec3d9019096c902953d183823062f81abba540b5
jamesprivategmail-commits <jamesprivategmail@gmail.com>
2026-07-21 23:33:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 require('dotenv').config();
 const { Telegraf } = require('telegraf');
+const express = require('express');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+const app = express();
 
-bot.start((ctx) => {
-  ctx.reply(`Welcome ${ctx.from.first_name}! 👋 I'm alive and ready.`);
-});
+app.get('/', (req, res) => res.send('Bot is alive'));
+app.listen(process.env.PORT || 3000, () => console.log('Web server running'));
 
-bot.command('help', (ctx) => {
-  ctx.reply('Available commands:\n/start\n/help\n/ping');
-});
-
+bot.start((ctx) => ctx.reply(`Welcome ${ctx.from.first_name}! 👋`));
+bot.command('help', (ctx) => ctx.reply('Available commands:\n/start\n/help\n/ping'));
 bot.command('ping', (ctx) => ctx.reply('pong 🏓'));
-
-bot.on('text', (ctx) => {
-  ctx.reply(`You said: "${ctx.message.text}"`);
-});
+bot.on('text', (ctx) => ctx.reply(`You said: "${ctx.message.text}"`));
 
 bot.launch();
 console.log('Bot is running...');
-
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
